ac288aeba7346c0d32a8aa38d7c08e61125116c3
Vishal R Setty <108011953+vishalsetty11@users.noreply.github.com>
2023-12-11 21:06:20 +0530

    Update Twosum.java

--- Twosum.java
+++ Twosum.java
@@ -3,15 +3,17 @@ class Solution
     public int[] twoSum(int[] nums, int target) 
     {
         int[] twosum = new int[2];
-        int i,inc=0;
+        int i,j;
         for(i=0;i<nums.length;i++)
         {
-            inc=i+1;
-            if(nums[i]+nums[inc]==target)
+            for(j=i+1;j<nums.length;j++)
             {
-                twosum[0]=i;
-                twosum[1]=inc;
-            return twosum;
+                if(nums[i]+nums[j]==target)
+                {
+                    twosum[0]=i;
+                    twosum[1]=j;
+                return twosum;
+                }
             }
         }
          return new int[2];   
